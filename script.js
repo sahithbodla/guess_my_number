a63864a808607a997fd8870a36759a844c659849
Sahith Bodla <sahith.bodla@gmail.com>
2021-09-29 13:36:01 +0530

    Refactored Code to adopt DRY principle

--- script.js
+++ script.js
@@ -26,28 +26,17 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.highscore').textContent = highscore;
         }
 
-        // When guess is too high!
-    } else if(guess > secretNumber) {
+        // When guess is wrong
+    } else {
         if(score>1){
-            document.querySelector('.message').textContent = "📈 Too High!";
+            document.querySelector('.message').textContent = guess>secretNumber?"📈 Too High!":"📉 Too Low!";
             score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = "😥 You lost the Game!";
             document.querySelector('.score').textContent = 0;
         }
-
-        // When guess is too low!
-    } else if(guess < secretNumber) {
-        if(score>1){
-            document.querySelector('.message').textContent = "📉 Too Low!";
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = "😥 You lost the Game!";
-            document.querySelector('.score').textContent = 0;
-        };
-    }
+    } 
 });
 
 document.querySelector('.again').addEventListener('click', function(){
