a0185a3ee3661eab3965888f7d590d2a416bcb47
Sahith Bodla <sahith.bodla@gmail.com>
2021-09-29 13:24:38 +0530

    Highscore Logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 
 document.querySelector('.check').addEventListener('click', function() {
@@ -14,13 +15,16 @@ document.querySelector('.check').addEventListener('click', function() {
 
         // When player wins
     } else if(guess === secretNumber) {
+
         document.querySelector('.message').textContent = "🎉 Correct Number!";
-
         document.querySelector('.number').textContent = secretNumber;
-
         document.querySelector('body').style.backgroundColor = "#60b347";
+        document.querySelector('.number').style.width = "30rem";
 
-        document.querySelector('.number').style.width = "30rem"
+        if(score>highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
         // When guess is too high!
     } else if(guess > secretNumber) {
